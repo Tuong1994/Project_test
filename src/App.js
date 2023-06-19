6b2ca3841be45470abe1b2ab777395593b8bfd5d
Nham Bon Tuong <nhambontuong68@gmail.com>
2023-06-19 23:11:21 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,54 +1,57 @@
-import { Fragment, useCallback, useRef, useState } from "react";
+import { Fragment, useRef, useState } from "react";
 import { data } from "./data";
-import FullCalendar from '@fullcalendar/react'
-import DatePicker from "react-datepicker"
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import interactionPlugin from '@fullcalendar/interaction'
-import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid';
+import FullCalendar from "@fullcalendar/react";
+import DatePicker from "react-datepicker";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import resourceTimeGridPlugin from "@fullcalendar/resource-timegrid";
 import moment from "moment";
 import "react-datepicker/dist/react-datepicker.css";
-import './App.css';
+import "./App.css";
 
 function App() {
-  const [eventsData, setEventsData] = useState(data);
+  const [eventsData, setEventsData] = useState(() => {
+    return data.map((data) => ({
+      id: data.event.id,
+      title: `${data.tile.header}<>${[
+        data.tile.content[0],
+        "-",
+        data.tile.content[1],
+      ].join(" ")}`,
+      start: data.event.start,
+      end: data.event.end,
+    }));
+  });
 
-  const [currentEvents, setCurrentEvents] = useState([]);
-
-  const [selectView, setSelectView] = useState("timeGridDay");
+  const [selectView, setSelectView] = useState("dayGridMonth");
 
   const [startDate, setStartDate] = useState(new Date());
 
-  const [eventUpdate, setEventUpdate] = useState("");
+  const [endDate, setEndDate] = useState(new Date());
 
   const calendarRef = useRef(null);
 
-  let eventGuid = 0
-
-  const events = useCallback(() => {
-    return eventsData.map((event) => (
-      {
-        id: createEventId(),
-        title: `${event.tile.header} ${event.tile.content[1]}`,
-        start: event.time_window.start,
-        end: event.time_window.end,
-      }
-    ))
-  }, [eventsData])
-
   const createEventId = () => {
-    return String(eventGuid++)
-  }
+    let eventGuid = 0;
+    return String(eventGuid++);
+  };
 
   const handleViewSelect = (e) => {
-    setSelectView(e.target.value)
-  }
+    setSelectView(e.target.value);
+    calendarRef.current.getApi().changeView(e.target.value);
+  };
+
+  const handleDatePick = (date) => {
+    setStartDate(date);
+    calendarRef.current.getApi().gotoDate(date);
+  };
 
   const handleDateSelect = (selectInfo) => {
-    let title = prompt('Please enter a new title for your event')
-    let calendarApi = selectInfo.view.calendar
+    let title = prompt("Please enter a new title for your event");
+    let calendarApi = selectInfo.view.calendar;
 
-    calendarApi.unselect() // clear date selection
+    calendarApi.unselect(); // clear date selection
 
     if (title) {
       calendarApi.addEvent({
@@ -56,25 +59,52 @@ function App() {
         title,
         start: selectInfo.startStr,
         end: selectInfo.endStr,
-        allDay: selectInfo.allDay
-      })
+        allDay: selectInfo.allDay,
+      });
     }
-  }
+  };
 
-  const handleEvents = (e) => {
-    setCurrentEvents(e)
-  }
+  const handleEventUpdate = (e) => {
+    const idx = eventsData.findIndex((event) => event.id === e.event.id);
+
+    eventsData[idx].title.split("<>")[1].split("-")[1] = moment(
+      e.event.start
+    ).format("MM/DD/YYYY");
+
+    eventsData[idx].start = moment(e.event.start).format();
+    eventsData[idx].end = moment(e.event.end).format();
+
+    console.log(eventsData[idx].title);
+
+    setEventsData(eventsData);
+  };
 
   const renderEventContent = (eventInfo) => {
-    return <div style={{ overflow: "hidden" }}>
-      <small>{eventInfo.event.title}</small>
-    </div>
-  }
+    const content = eventInfo.event.title.split("<>");
+    return (
+      <div
+        style={{
+          width: "100%",
+          padding: "10px",
+          overflow: "hidden",
+          borderRadius: "4px",
+        }}
+      >
+        <small>{content[0]}</small>
+        <br />
+        <small>{content[1]}</small>
+      </div>
+    );
+  };
 
   return (
     <Fragment>
-      <div>
-        <select onChange={handleViewSelect}>
+      <div className="fc-header">
+        <select
+          className="fc-view-select"
+          value={selectView}
+          onChange={handleViewSelect}
+        >
           <option value="timeGridDay">day</option>
           <option value="timeGridWeek">week</option>
           <option value="dayGridMonth">month</option>
@@ -82,30 +112,32 @@ function App() {
 
         <DatePicker
           monthsShown={2}
+          selected={startDate}
+          onChange={handleDatePick}
         />
       </div>
 
       <FullCalendar
         ref={calendarRef}
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, resourceTimeGridPlugin]}
-        headerToolbar={{
-          left: "",
-          center: "",
-          right: "timeGridDay timeGridWeek dayGridMonth"
-        }}
+        plugins={[
+          dayGridPlugin,
+          timeGridPlugin,
+          interactionPlugin,
+          resourceTimeGridPlugin,
+        ]}
+        headerToolbar={false}
         editable={true}
         selectable={true}
         selectMirror={true}
         dayMaxEvents={true}
         weekends={true}
-        initialView='dayGridMonth'
         views={[selectView]}
-        initialEvents={events()} // alternatively, use the `events` setting to fetch from a feed
+        initialEvents={eventsData}
         select={handleDateSelect}
-        eventsSet={handleEvents} // called after events are initialized/added/changed/removed
         eventContent={renderEventContent} // custom render function
-        eventChange={(e) => console.log(e)}
-      /* you can update a remote database when these fire:
+        eventResize={handleEventUpdate}
+        eventChange={handleEventUpdate}
+        /* you can update a remote database when these fire:
       eventChange={() => {}}
     eventAdd={function(){}}
     eventRemove={function(){}}
